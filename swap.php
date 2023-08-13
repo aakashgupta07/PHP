f21ca6b689486771d15019dccafdf04e71c53213
Aakash Gupta <aakashhprof@gmail.com>
2023-08-14 00:00:58 +0530

    first commit

---
+++ swap.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>First Scipt</title>
+</head>
+<body>
+  <?php
+  $a=5;
+  $b=7;
+  $c=$a;
+  $a=$b;
+  $b=$c;
+    echo"a=$a and b=$b";
+  ?>
+</body>
+</html>
