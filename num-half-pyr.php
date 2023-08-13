47bd4c4c33d4d07a4d88477da6c66515795aec84
Aakash Gupta <aakashhprof@gmail.com>
2023-08-14 00:21:07 +0530

    first commit

---
+++ num-half-pyr.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>First Scipt</title>
+</head>
+<body>
+  <?php
+  for($row=0;$row<5;$row++)
+  {
+    for($col=0;$col<$row+1;$col++)
+    {
+      $temp=$col+1;
+      echo "$temp";
+    }
+    
+  }
+  ?>
+</body>
+</html>
