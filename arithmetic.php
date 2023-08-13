c92dca5a515339ccd859c350e6b04303c510b283
Aakash Gupta <aakashhprof@gmail.com>
2023-08-14 00:20:27 +0530

    first commit

---
+++ arithmetic.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>First Scipt</title>
+</head>
+<body>
+  <?php
+    $a=35;
+    $b=7;
+    $sum=$a+$b;
+    $sub=$a-$b;
+    $mul=$a*$b;
+    $quo=$a/$b;
+    $rem=$a%$b;
+    $pow=$a**$b;
+    echo "Addition = $sum";
+    echo "<br>Substraction=$sub";
+    echo "<br>Multiplication=$mul";
+    echo "<br>Quotient=$quo";
+    echo "<br>Reminder=$rem";
+    echo "<br>Exponential=$pow";
+    ?>
+</body>
+</html>
