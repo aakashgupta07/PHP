73e666b47222701a25f7ad27475b4254b043d270
Aakash Gupta <aakashhprof@gmail.com>
2023-08-13 23:58:17 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>First Scipt</title>
+</head>
+<body>
+  <?php
+    echo"Namaste";
+    echo"<br>Welcome!!"
+  ?>
+</body>
+</html>
