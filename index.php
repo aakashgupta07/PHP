263ac501a0c5717a174b14e2ed6bb97c824b4087
Aakash Gupta <aakashhprof@gmail.com>
2023-08-19 22:09:18 +0530

    edit syntax

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title></title>
-</head>
-<body>
-  <?php
+<?php
     echo"Namaste";
     echo"<br>Welcome!!"
   ?>
-</body>
-</html>
