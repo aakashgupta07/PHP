036681c71e85dcf1f2785c958751d38f2ed66b9b
Aakash Gupta <aakashhprof@gmail.com>
2023-08-25 23:35:11 +0530

    display a prime number from an array

---
+++ prime.php
@@ -0,0 +1,22 @@
+<?php
+  function check_prime($num)
+  {
+    if($num==1)
+    return 0;
+
+    for($i=1;$i<$num-1;$i++)
+    {
+      if($num%$i==0)
+      return 0;
+    }
+    return 1;
+  }
+  echo "Prime numbers <br>";
+  $arr=array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
+  for($i=0;$i<count($arr);$i++)
+  {
+    $flag=check_prime($arr[$i]);
+    if($flag==1)
+    echo "$arr[$i]<br>";
+  }
+?>
